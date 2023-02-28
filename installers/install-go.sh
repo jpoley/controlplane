09dd114dc12d7760acc3f91db5472c80f7389f72
jason poley <jason.poley@gmail.com>
2023-02-27 21:52:51 -0500

    few updates

--- installers/install-go.sh
+++ installers/install-go.sh
@@ -1,7 +1,7 @@
 echo "Installing Go"
 
-wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz 
-tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+wget -q https://go.dev/dl/go1.20.1.linux-amd64.tar.gz 
+tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
 echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 cd ~
 rm -rf go1.18.3.linux-amd64.tar.gz 
